ff50a4c385cb06fda2955b5b614fe999ad565d8a
tennyson <lovelytrump@outlook.com>
2024-07-16 00:55:55 +0800

    add mutil env

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    server: {
+      port: env.VITE_PORT as any,
+      open: env.VITE_OPEN as string
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
